b32832b2beb5ec4870cbe914d0ecdf644b078b3a
Anita <a.nita@hotmail.co.uk>
2016-05-18 16:55:31 +0100

    created container WeatherList and connect it to redux see https://github.com/heron2014/weather-redux-app/issues/7

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-
+// action fetchWather is making ajax request 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},uk`;
   const request = axios.get(url);
-  
+
   return {
     type: FETCH_WEATHER,
     payload: request
